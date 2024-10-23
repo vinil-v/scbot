88cd1e7b1b11de7eea00493226ed51268b77d784
Vinil Vadakkepurakkal <82436391+vinil-v@users.noreply.github.com>
2024-10-23 11:05:14 +0530

    appupdate

--- flaskapp.wsgi
+++ flaskapp.wsgi
@@ -1,4 +1,13 @@
 import sys
+import os
+
+# Activate virtual environment
+activate_this = '/var/www/flaskapp/venv/bin/activate_this.py'
+with open(activate_this) as file_:
+    exec(file_.read(), dict(__file__=activate_this))
+
+# Add the app directory to the sys.path
 sys.path.insert(0, '/var/www/flaskapp')
 
+# Import the Flask app
 from app import app as application
--- setup.bash
+++ setup.bash
@@ -1,15 +1,22 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get install apache2 libapache2-mod-wsgi-py3 python3-pip
+sudo apt-get install python3-venv
+
 pip3 install flask openai requests beautifulsoup4
 
 sudo mkdir -p /var/www/flaskapp
+
 sudo chmod -R 755 /var/www/flaskapp
 sudo cp app.py /var/www/flaskapp/
 sudo cp flaskapp.wsgi /var/www/flaskapp/
-
+cd /var/www/flaskapp
+sudo python3 -m venv venv
+source venv/bin/activate
+pip3 install flask openai requests beautifulsoup4
 sudo chown -R www-data:www-data /var/www/flaskapp
 sudo cp flaskapp.conf /etc/apache2/sites-available/flaskapp.conf
+sudo a2enmod wsgi
 sudo a2ensite flaskapp
 sudo a2dissite 000-default
 sudo apache2ctl configtest
